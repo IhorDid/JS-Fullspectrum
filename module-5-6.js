76f7707e0009b8c08e3db257f59074f84692bd9d
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-03-03 20:03:15 +0200

    fix

---
+++ module-5-6.js
@@ -0,0 +1,23 @@
+//TODO: № 1 на this ✅ ==============================================
+// Виправте помилки, щоб код працював
+// const product = {
+//   price: 5000,
+//   showPrice() {
+//     console.log(this.price);
+//   },
+// };
+// const price = 1000;
+// product.showPrice();
+
+//TODO: № 2 на перебирання ✅ ==============================================
+//Знайти унікальні елементи за допомогою reduce
+// const numbers = [1, 9, 0, 1, 5, 9, 1, 6];
+// const numbersFun = (array) =>
+//   array.reduce(
+//     (acc, number) =>
+//   array.indexOf(number) === ind;
+//   return ;
+//       acc.includes(number) ? acc : [...acc, number],
+//     []
+//   );
+// console.log(numbersFun(numbers));
