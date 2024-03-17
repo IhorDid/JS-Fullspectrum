f1646d05e1b9a0c988c76647f65ca14edb89796c
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-03-17 19:47:37 +0200

    add

--- module-7-8.js
+++ module-7-8.js
@@ -2,10 +2,31 @@
 // Натискання на кнопку "SHOW ME" має
 // виводити значення з поля введення(дивіться на елементи в html - розмітці)
 
-const input = document.querySelector('#alertInput');
-const button = document.querySelector('#alertButton');
+// const input = document.querySelector('#alertInput');
+// const button = document.querySelector('#alertButton');
 
-button.addEventListener('click', showInput)
-function showInput() {
-   console.log(input.value);
-}
+// button.addEventListener('click', showInput)
+// function showInput() {
+//    console.log(input.value);
+// }
+
+
+//TODO: № 2 ==============================================
+// Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
+// Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
+
+// const leftInput = document.querySelector('#leftSwapInput'); 
+// const rightInput = document.querySelector('#rightSwapInput');
+// const button = document.querySelector('#swapButton');
+
+// button.addEventListener('click', swapInput);
+
+// function swapInput() {
+//    const leftInputText = leftInput.value;
+//    leftInput.value = rightInput.value;
+//    rightInput.value = leftInputText;
+// };
+
+
+
+
