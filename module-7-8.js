7c6f46faf3d394bd68e7d5bc0fba17ea59ef6d61
IhorDid <101325065+IhorDid@users.noreply.github.com>
2024-03-18 22:24:39 +0200

    practica

--- module-7-8.js
+++ module-7-8.js
@@ -68,3 +68,15 @@
 //   }
 // };
 // input.addEventListener("blur", rec);
+
+const input = document.querySelector("#validation-input");
+const rec = (e) => {
+  if (e.currentTarget.value.length === Number(e.currentTarget.dataset.length)) {
+    input.classList.remove("invalid");
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+};
+input.addEventListener("blur", rec);
